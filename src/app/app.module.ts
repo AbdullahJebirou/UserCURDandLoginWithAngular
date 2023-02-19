fcebf5c798cb140fa2ebe36dd8ae542a592c2525
Abdullah <81176026+Abdullah023313@users.noreply.github.com>
2023-02-19 23:57:32 +0300

    Add-code-to-app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import { TranslateModule } from '@ngx-translate/core';
+import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { AuthGuard } from './user/guards/auth.guard';
@@ -11,7 +13,14 @@ import { AuthGuard } from './user/guards/auth.guard';
   imports: [
     BrowserModule,
     SharedModule,
-    TranslateModule.forRoot(),
+    TranslateModule.forRoot({
+      defaultLanguage: 'en',
+      loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+      }
+  }),
     RouterModule.forRoot([
       {
         path: 'User',
@@ -32,3 +41,7 @@ import { AuthGuard } from './user/guards/auth.guard';
   bootstrap: [AppComponent],
 })
 export class AppModule { }
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
